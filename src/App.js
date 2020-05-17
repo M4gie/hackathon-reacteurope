c05dc16a7f08edbe427dbbc3e8db939e939c5ba5
m4gie <steeven.regnault@epitech.eu>
2020-05-17 15:09:52 +0200

    add: check if style is working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppRouter from "./router";
+import "@progress/kendo-theme-default/dist/all.css";
 
 function App() {
   return <AppRouter />;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Button } from "@progress/kendo-react-buttons";
+import styled from "styled-components";
 
 export default function Home() {
-  return <>Hello Home</>;
+  return (
+    <Container>
+      <h1>Hello World</h1>
+      <Button primary>Click me</Button>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
