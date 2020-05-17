8521e392cab3cdc0459d232e72f44bd45403f6ba
m4gie <steeven.regnault@epitech.eu>
2020-05-17 15:33:25 +0200

    add: router handle setup page

--- src/router.js
+++ src/router.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Setup from "./pages/Setup";
 
 export default function AppRouter() {
   return (
     <Router>
       <div>
         <Switch>
+          <Route path="/setup">
+            <Setup />
+          </Route>
           <Route path="/">
             <Home />
           </Route>
