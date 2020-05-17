e43c7c1713a69ee51c39a7288635d81458f6cfa8
m4gie <steeven.regnault@epitech.eu>
2020-05-17 20:48:08 +0200

    add: SelectCategory component

---
+++ src/components/SelectCategory.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Button } from "@progress/kendo-react-buttons";
+import { useRecoilState } from "recoil";
+import { categoriesState } from "../recoil/categories";
+import { removeItem } from "../utils/array";
+
+export default function SelectCategory(props) {
+  const { category } = props;
+  const [gameCategories, setGameCategories] = useRecoilState(categoriesState);
+
+  function handleCategory(category) {
+    if (gameCategories.includes(category)) {
+      const index = gameCategories.findIndex(
+        (listItem) => listItem === category
+      );
+      const newCategories = removeItem(gameCategories, index);
+      setGameCategories(newCategories);
+    } else {
+      setGameCategories([...gameCategories, category]);
+    }
+  }
+
+  return (
+    <Button
+      type="button"
+      key={category}
+      primary={gameCategories.includes(category)}
+      onClick={() => handleCategory(category)}
+      {...props}
+    >
+      {category}
+    </Button>
+  );
+}
