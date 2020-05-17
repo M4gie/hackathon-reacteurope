ed941d336c29f139a74769316dab1524a149da0f
m4gie <steeven.regnault@epitech.eu>
2020-05-17 20:48:37 +0200

    add: SelectTime component

---
+++ src/components/SelectTime.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Slider, SliderLabel } from "@progress/kendo-react-inputs";
+import { useRecoilState } from "recoil";
+import { maxTimeState } from "../recoil/maxTime";
+
+export default function SelectTime() {
+  const [maxTime, setMaxTime] = useRecoilState(maxTimeState);
+
+  return (
+    <Slider
+      value={maxTime}
+      onChange={({ value }) => setMaxTime(value)}
+      min={1}
+      max={9}
+      step={1}
+      defaultValue={3}
+    >
+      {[1, 3, 5, 7, 9].map((time) => (
+        <SliderLabel
+          key={time}
+          position={time}
+          onClick={() => {
+            setMaxTime(time);
+          }}
+        >
+          {time}
+        </SliderLabel>
+      ))}
+    </Slider>
+  );
+}
---
+++ src/recoil/maxTime.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const maxTimeState = atom({
+  key: "maxTimeState",
+  default: 3,
+});
