9cd4aad192227fbbc9c06b6a82a2cabafdb97826
m4gie <steeven.regnault@epitech.eu>
2020-05-18 10:07:40 +0200

    add: display categories input in game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,22 +1,31 @@
 import React from "react";
 import Container from "../components/Container";
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import { categoriesState } from "../recoil/categories";
 import Link from "../components/Link";
 import { Button } from "@progress/kendo-react-buttons";
+import { Input } from "@progress/kendo-react-inputs";
+import { gameLetterState } from "../recoil/letter";
 
 export default function Game() {
-  const gameCategories = useRecoilState(categoriesState);
+  const gameCategories = useRecoilValue(categoriesState);
+  const gameLetter = useRecoilValue(gameLetterState);
+
   return (
     <Container>
       <div>
         {gameCategories.map((category) => (
-          <p>{category}</p>
+          <p>
+            <Input defaultValue={gameLetter} label={category} />
+          </p>
         ))}
-      </div>
-      <div>
         <Link to="/">
           <Button type="button">Return home</Button>
+        </Link>
+        <Link to="/result">
+          <Button type="button" primary>
+            Finished !
+          </Button>
         </Link>
       </div>
     </Container>
--- src/recoil/categories.js
+++ src/recoil/categories.js
@@ -2,5 +2,5 @@ import { atom } from "recoil";
 
 export const categoriesState = atom({
   key: "categoriesState",
-  default: [],
+  default: ["Name", "City", "Vegetable"],
 });
