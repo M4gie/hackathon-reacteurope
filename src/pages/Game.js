17cb682e5ce2a5e55f5ba3c7a4969e14bab98060
m4gie <steeven.regnault@epitech.eu>
2020-05-18 11:13:20 +0200

    add: game timer

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -5,28 +5,60 @@ import { categoriesState } from "../recoil/categories";
 import Link from "../components/Link";
 import { Button } from "@progress/kendo-react-buttons";
 import { Input } from "@progress/kendo-react-inputs";
+import Timer from "react-compound-timer";
 import { gameLetterState } from "../recoil/letter";
+import { convertedMaxTimeState } from "../recoil/maxTime";
 
 export default function Game() {
   const gameCategories = useRecoilValue(categoriesState);
   const gameLetter = useRecoilValue(gameLetterState);
+  const convertedMaxTime = useRecoilValue(convertedMaxTimeState);
+
+  function handleEndGame(remainingTime) {
+    console.log("remaining time: ", remainingTime);
+  }
 
   return (
     <Container>
       <div>
-        {gameCategories.map((category) => (
-          <p>
-            <Input defaultValue={gameLetter} label={category} />
-          </p>
-        ))}
-        <Link to="/">
-          <Button type="button">Return home</Button>
-        </Link>
-        <Link to="/result">
-          <Button type="button" primary>
-            Finished !
-          </Button>
-        </Link>
+        <Timer
+          initialTime={convertedMaxTime}
+          direction="backward"
+          formatValue={(value) => `${value < 10 ? `0${value}` : value}`}
+          checkpoints={[
+            {
+              time: 0,
+              callback: () => handleEndGame(0),
+            },
+          ]}
+        >
+          {({ getTime, stop }) => (
+            <>
+              <div>
+                <Timer.Minutes />:
+                <Timer.Seconds />
+              </div>
+              {gameCategories.map((category) => (
+                <p key={category}>
+                  <Input defaultValue={gameLetter} label={category} />
+                </p>
+              ))}
+              <Link to="/">
+                <Button type="button">Return home</Button>
+              </Link>
+              <Button
+                type="button"
+                primary
+                onClick={() => {
+                  stop();
+                  handleEndGame(getTime());
+                }}
+              >
+                Finished !
+              </Button>
+            </>
+          )}
+        </Timer>
       </div>
     </Container>
   );
