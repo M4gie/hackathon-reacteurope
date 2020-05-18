6d674676eab31cd0cf80530474c33fca912faea0
m4gie <steeven.regnault@epitech.eu>
2020-05-18 14:55:29 +0200

    add: save answers

---
+++ src/components/Answer.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Input } from "@progress/kendo-react-inputs";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { gameLetterState } from "../recoil/letter";
+import { answersState } from "../recoil/answers";
+import { replaceItemAtIndex } from "../utils/array";
+
+export default function Answer(props) {
+  const { category } = props;
+  const gameLetter = useRecoilValue(gameLetterState);
+  const [answers, setAnswers] = useRecoilState(answersState);
+  let inputIndex = answers.findIndex(
+    (listItem) => listItem.category === category
+  );
+
+  function handleInput(text) {
+    if (inputIndex !== -1) {
+      const newAnswers = replaceItemAtIndex(answers, inputIndex, {
+        category,
+        text,
+      });
+      setAnswers(newAnswers);
+    } else {
+      setAnswers([...answers, { category, text }]);
+    }
+  }
+
+  return (
+    <p>
+      <Input
+        label={category}
+        value={inputIndex !== -1 ? answers[inputIndex].text : gameLetter}
+        onChange={({ target: { value } }) => handleInput(value)}
+      />
+    </p>
+  );
+}
