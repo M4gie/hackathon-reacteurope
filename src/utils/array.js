7939b1c5546191ea9915957589386b0b0ab2ec02
m4gie <steeven.regnault@epitech.eu>
2020-05-18 14:55:56 +0200

    add: array method to edit element

--- src/utils/array.js
+++ src/utils/array.js
@@ -1,3 +1,7 @@
 export function removeItem(arr, index) {
   return [...arr.slice(0, index), ...arr.slice(index + 1)];
 }
+
+export function replaceItemAtIndex(arr, index, newValue) {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
