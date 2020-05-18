d9bef03760c3202e23d8484fe00d3bca86311142
m4gie <steeven.regnault@epitech.eu>
2020-05-18 11:12:53 +0200

    add: selector to convert time

--- src/recoil/maxTime.js
+++ src/recoil/maxTime.js
@@ -1,6 +1,14 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const maxTimeState = atom({
   key: "maxTimeState",
   default: 3,
+});
+
+export const convertedMaxTimeState = selector({
+  key: "convertedMaxTimeState",
+  get: ({ get }) => {
+    const maxTime = get(maxTimeState);
+    return maxTime * 60000;
+  },
 });
