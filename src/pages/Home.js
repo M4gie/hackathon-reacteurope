146ed1c010cc88127b53f883be758e7b6942dd5e
m4gie <steeven.regnault@epitech.eu>
2020-05-19 11:30:11 +0200

    add: home style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { Button } from "@progress/kendo-react-buttons";
 import Container from "../components/Container";
 import Link from "../components/Link";
+import Title from "../components/Title";
+import Button from "../components/Button";
 
 export default function Home() {
   return (
     <Container>
-      <Link to="/setup">
-        <Button primary>Create a game</Button>
-      </Link>
+      <div>
+        <Title>Find the word...</Title>
+        <Link to="/setup">
+          <Button primary>Create a game</Button>
+        </Link>
+      </div>
     </Container>
   );
 }
