0f881fdc7e74aac1739dc3fba793b77dc71123c9
m4gie <steeven.regnault@epitech.eu>
2020-05-19 14:24:41 +0200

    add: style to stats page

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { Button } from "@progress/kendo-react-buttons";
-
 import Container from "../components/Container";
 import Link from "../components/Link";
 import TimeStats from "../components/TimeStats";
 import AnswerStats from "../components/AnswersStats";
 import Score from "../components/Score";
+import Button from "../components/Button";
 
 export default function Stats() {
   return (
     <Container>
-      <Score />
-      <TimeStats />
-      <AnswerStats />
-      <Link to="/setup">
-        <Button>Edit config</Button>
-      </Link>
-      <Link to="/game">
-        <Button primary>Play again !</Button>
-      </Link>
+      <div>
+        <Score />
+        <div style={{ display: "flex" }}>
+          <TimeStats />
+          <AnswerStats />
+        </div>
+        <Link to="/setup">
+          <Button margin>Edit config</Button>
+        </Link>
+        <Link to="/game">
+          <Button primary margin>
+            Play again !
+          </Button>
+        </Link>
+      </div>
     </Container>
   );
 }
